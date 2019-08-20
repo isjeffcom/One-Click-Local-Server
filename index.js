b130822363762a95ed45d6f1672263cfa1cc782e
JIANFENG WU <isjeffcom@gmail.com>
2019-08-20 23:56:51 +0800

    first push

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var fs = require('fs')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// 访问静态资源
+app.use(express.static('./dist'))
+
+// 访问单页
+app.get('*', function (req, res) {
+    var html = fs.readFileSync('./dist/index.html', 'utf-8')
+    res.send(html)
+});
+
+// 监听
+app.listen(8081, function () {
+    console.log('Penrose 2D本地启动成功，项目运行在：' + 'http://localhost:8081')
+});
+
+var exec = require('child_process').exec;
+var url= "http://localhost:8081/"
+switch (process.platform) {
+  case "darwin":
+    exec('open ' + url);
+    break;
+  case "win32":
+    exec('start ' + url);
+    break;
+  default:
+    exec('xdg-open', [url]);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+npm run pkg
+
+
+挂载dist文件夹里的静态页，反正vue是没问题的
